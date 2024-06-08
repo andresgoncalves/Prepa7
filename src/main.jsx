5b9f14c793cc30e0b9c7e5b92c2e0d98433f9323
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2024-06-08 16:37:27 -0400

    Firebase init

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
-import React, { createContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './Home';
-import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CardsPage from './CardsPage';
-import { NuevoContextProvider } from './NuevoContexto';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NuevoContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home titulo={5} />} />
-          <Route path="/cards" element={<CardsPage />} />
-        </Routes>
-      </BrowserRouter>
-    </NuevoContextProvider>
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
